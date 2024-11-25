6935e04cb167732633d70526e74884ac853c2af3
mariih02 <marianasilvasantana90@gmail.com>
2024-11-25 12:03:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Qual o munumento famoso pela sua inclinação?',
+    'Torre de pisa'
+)
+
+criaCartao(
+    'Geografia',
+    'Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?',
+    'Aproximadamente 24 horas'
+)
+
+criaCartao(
+    'Fisica',
+    'A que temperatura a água ferve?',
+    '100 c'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a maior floresta tropical do mundo?',
+    'Flresta Amazônica'
+)
